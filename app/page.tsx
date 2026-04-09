b0eba986cfe7262519287ed1092ad82388fd3d31
ZENG,ROU-QI <111508016@g.nccu.edu.tw>
2026-04-09 23:40:45 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,13 @@ export default function Home() {
       <div className="mx-auto flex h-screen max-w-6xl">
         {/* 左側固定 */}
         <aside className="sticky top-0 h-screen w-[320px] overflow-y-auto border-r border-black/10 bg-white/70 p-6 backdrop-blur">
-          <div className="text-xl font-black">曾柔綺</div>
-          <div className="mt-1 text-sm text-black/70">數位內容與科技雙主修</div>
+          <div className="text-xl font-black text-center">曾柔綺</div>
+          <div className="mt-1 text-sm text-black/70 text-center">數位內容與科技雙主修</div>
+          <div className="flex justify-center">
+            <div>instagram</div>
+            <div>facebook</div>
+            <div>github</div>
+          </div>
 
 
           <div className="mt-3 grid grid-cols-2 gap-3"> </div>
