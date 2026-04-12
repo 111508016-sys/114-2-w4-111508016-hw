45ca432e761a40b458069a6389aa3d42989ad14f
ZENG,ROU-QI <111508016@g.nccu.edu.tw>
2026-04-12 21:27:03 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div>
       <HomePhotoTrail />
-      <div className="mt-6 text-3xl font-black">首頁</div>
-      <div className="mt-2 text-black/70">lifestyle photos (hover to view)</div>
+      <div className="mt-6 text-3xl font-black">lifestyle photos</div>
+      <div className="mt-2 text-black/70">移動鼠標可查看</div>
     </div>
   );
 }
